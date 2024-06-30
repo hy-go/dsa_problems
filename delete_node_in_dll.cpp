6698b0330d890028fceb98ffeb13269d6c3622fa
Harish Yadav <157452802+hy-go@users.noreply.github.com>
2024-06-30 15:59:36 +0530

    Create delete_node_in_dll.cpp

---
+++ delete_node_in_dll.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int x) {
+        if(x == 1) {
+            if(head->next)
+                head->next->prev = NULL;
+                return head->next;
+        }
+        head->next = deleteNode(head->next, x-1);
+        return head;
+    }
+};
+
+void printList(struct Node *node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    int t, x, n, i;
+    scanf("%d", &t);
+
+    while (t--) {
+        /* Start with the empty list */
+        struct Node *temp, *head = NULL;
+        scanf("%d", &n);
+
+        temp = NULL;
+
+        for (i = 0; i < n; i++) {
+            scanf("%d", &x);
+
+            if (head == NULL) {
+                head = new Node(x);
+                temp = head;
+            } else {
+                Node *temp1 = new Node(x);
+                temp->next = temp1;
+                temp1->prev = temp;
+                temp = temp->next;
+            }
+        }
+
+        scanf("%d", &x);
+
+        Solution ob;
+        head = ob.deleteNode(head, x);
+
+        printList(head);
+        while (head->next != NULL) {
+            temp = head;
+            head = head->next;
+            free(temp);
+        }
+
+        free(head);
+    }
+    return 0;
+}
